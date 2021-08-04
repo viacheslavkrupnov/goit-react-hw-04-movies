d52e12447d42e508bef3e17e914eb70e3df8f4b8
viacheslavkrupnov <slava86krupnov@gmail.com>
2021-08-04 16:33:00 +0400

    render home page

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import moviesApi from '../../services/moviesApi';
+import s from './HomePage.module.css';
+
+export default function HomePage() {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const srcBaseUrl = 'https://image.tmdb.org/t/p/w500';
+  const location = useLocation();
+
+  useEffect(() => {
+    const renderTrendingMovies = () => {
+      moviesApi
+        .fetchTrendingMovies()
+        .then(response => setTrendingMovies(response));
+    };
+    renderTrendingMovies();
+  }, []);
+
+  return (
+    <>
+      <h2 className={s.title}>Trending Today</h2>
+      <ul className={s.filmsList}>
+        {trendingMovies.map(({ poster_path, title, id }) => (
+          <li key={id} className={s.filmsListItem}>
+            <Link
+              to={{
+                pathname: `/movies/${id}`,
+                state: {
+                  from: location.pathname,
+                },
+              }}
+              className={s.link}
+            >
+              <img
+                className={s.image}
+                src={`${srcBaseUrl}${poster_path}`}
+                alt=""
+              />
+              <h3 className={s.movieTitle}>{title}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
