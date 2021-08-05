0a230956c5709e5d2ff8a0dca82cca4a1c2f8dcc
viacheslavkrupnov <slava86krupnov@gmail.com>
2021-08-05 21:39:54 +0400

    connect component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import AppBar from './components/AppBar';
 import Container from './components/Container';
 
 const HomePage = lazy(() =>
   import('./components/HomePage' /* webpackChunkName: "HomePage" */),
+);
+const MoviesPage = lazy(() =>
+  import('./components/MoviesPage' /* webpackChunkName: "MoviesPage" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    './components/MovieDetailsPage' /* webpackChunkName: "MovieDetailsPage" */
+  ),
 );
 
 function App() {
@@ -17,8 +29,18 @@ function App() {
           <Route path="/" exact>
             <HomePage />
           </Route>
+
+          <Route path="/movies" exact>
+            <MoviesPage />
+          </Route>
+
+          <Route path="/movies/:movieId">
+            <MovieDetailsPage />
+          </Route>
         </Switch>
       </Suspense>
+
+      <ToastContainer autoClose={3000} position="top-right" />
     </Container>
   );
 }
